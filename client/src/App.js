4b46de3e7b7b40bad68364d4e1580e61d89231c9
gisung3253 <gisung3253@naver.com>
2024-09-29 13:18:28 +0900

    lec5

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,20 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from './pages/CreatePost';
+import Post from "./pages/Post"
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Link to="/createpost">Create A Post</Link>
-        <Link to="/">Home page</Link>
+        <div className="navbar">
+          <Link to="/createpost">Create A Post</Link>
+          <Link to="/">Home page</Link>
+        </div>
         <Routes>
           <Route path="/" exact element={<Home/>} />
           <Route path="/createpost" exact element={<CreatePost/>} />
+          <Route path="/post/:id" exact element={<Post/>} />
         </Routes>
       </Router>
     </div>
--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage} from "formik";
 import * as Yup from 'yup'
 import axios from "axios";
+import { useNavigate } from 'react-router-dom'
 
 function CreatePost() {
 
@@ -17,9 +18,11 @@ function CreatePost() {
         username: Yup.string().min(3).max(15).required(),
     })
 
+    let navigate = useNavigate()
+
     const onSubmit = (data) => {
         axios.post("http://localhost:3001/posts", data).then((response) => {
-            console.log("IT WORKED");
+            navigate("/");
         })
     }
 
